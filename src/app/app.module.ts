2a34704ce577380a3b2e11f50647f2829d17ce21
ZibaloV <fotoroom.md@gmail.com>
2020-04-20 18:51:49 +0300

    add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
-// import { Post2Component } from './post2/post2.component';
+//import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-   // Post2Component
+    // Post2Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
