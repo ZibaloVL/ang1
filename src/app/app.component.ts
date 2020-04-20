284e650f0ba140615f232151d2d8d9947139a621
ZibaloV <fotoroom.md@gmail.com>
2020-04-20 08:00:37 +0300

    ццц

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ang1';
+  title = 'apppppppppppp';
+  titleNg ='ngngngngn'
+  inputValue = '';
+
+  onInput(event?) {
+
+    this.inputValue = event.target.value;
+  }
+
+  onInput2(event: any) {
+
+    this.title = event.target.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    Post2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
