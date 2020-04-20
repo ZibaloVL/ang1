710cba9942a73f77b079e026ce53391f6e925688
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2020-04-20 10:37:18 +0300

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'apppppppppppp';
-  titleNg = 'ngngngngn';
-  inputValue = '';
-
-  onInput(event?) {
-
-    this.inputValue = event.target.value;
-  }
-
-  onInput2(event: any) {
-
-    this.title = event.target.value;
-  }
+  backgroundTogle = false;
+  classTogle = true;
 }
