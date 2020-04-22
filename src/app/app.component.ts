722bcd6ba808da2f3338ed5067cac48595eb91b3
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2020-04-22 07:11:46 +0300

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: String,
+  text: String,
+  id?: number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+ posts: Post[] = [
+   {title: "Lord", text: "Stars", id: 1 },
+   {title: "Lord2", text: "Stars2", id: 2 }
+ ]
 }
