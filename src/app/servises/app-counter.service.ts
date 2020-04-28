d0c9d56ea7a33f761fd2446c5dfa190ff6a04f34
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2020-04-28 06:09:01 +0300

    servises_end

--- src/app/servises/app-counter.service.ts
+++ src/app/servises/app-counter.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { LogService } from './log.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppCounterService {
-
+  
   count = 0;
 
+  constructor( public logService: LogService ) {}
+
   increase() {
+    this.logService.log('increase counter');
     this.count++;
   }
   decriase() {
